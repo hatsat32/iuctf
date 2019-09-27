e4271b410ec9790717d28b24c6a01ccbd7513bdf
Süleyman ERGEN <suleymanergen32@gmail.com>
2019-09-28 00:10:28 +0300

    make admin command added

---
+++ app/Commands/MakeAdmin.php
@@ -0,0 +1,53 @@
+<?php namespace App\Commands;
+
+use CodeIgniter\CLI\BaseCommand;
+use CodeIgniter\CLI\CLI;
+use CodeIgniter\Config\Services;
+use \App\Models\UserModel;
+
+class MakeAdmin extends BaseCommand
+{
+	protected $group       = 'Iuctf';
+	protected $name        = 'iuctf:makeadmin';
+	protected $description = 'Make a user an admin';
+	protected $usage     = "iuctf:makeadmin [USERNAME | EMAIL]";
+	protected $arguments = [
+		'username'		=> 'Username of the user',
+		'email'			=> 'Email of the user',
+	];
+
+	public function run(array $params)
+	{
+		$db = db_connect();
+		$auth  = Services::authorization();
+
+		// consume or prompt for group name
+		$identity = array_shift($params);
+		if (empty($identity))
+		{
+			$identity = CLI::prompt('Email', null, 'required');
+		}
+
+		$users = new UserModel();
+		$user = null;
+
+		if (filter_var($identity, FILTER_VALIDATE_EMAIL))
+		{
+			$user = $users->where('email', $identity)->first();
+		}
+		else
+		{
+			$user = $users->where('username', $identity)->first();
+		}
+
+		try
+		{
+			$auth->addUserToGroup($user['id'], 'admin');
+			CLI::write('Success!', 'yellow');
+		}
+		catch (\Exception $e)
+		{
+			$this->showError($e);
+		}
+	}
+}
