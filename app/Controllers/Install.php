6ca1c2cad62c83635b7318ef655f776758403b29
Süleyman ERGEN <hatsat32@protonmail.com>
2020-01-14 22:46:11 +0300

    Install controller base controller changed

--- app/Controllers/Install.php
+++ app/Controllers/Install.php
@@ -1,8 +1,9 @@
 <?php namespace App\Controllers;
 
+use CodeIgniter\Controller;
 use Config\Services;
 
-class Install extends BaseController
+class Install extends Controller
 {
 	//--------------------------------------------------------------------
 
