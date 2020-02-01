309c4327d9936bb0254755c8bdcd5fcd5108a9e8
Süleyman ERGEN <hatsat32@protonmail.com>
2020-02-01 22:41:40 +0300

    preparements for database tests

---
+++ tests/_support/DatabaseTestCase.php
@@ -0,0 +1,39 @@
+<?php namespace Tests\Support;
+
+use CodeIgniter\Test\CIDatabaseTestCase;
+
+class DatabaseTestCase extends CIDatabaseTestCase
+{
+	/**
+	 * Should the database be refreshed before each test?
+	 *
+	 * @var boolean
+	 */
+	protected $refresh = true;
+
+	/**
+	 * The name of a seed file used for all tests within this test case.
+	 *
+	 * @var string
+	 */
+	protected $seed = 'Tests\Support\Database\Seeds\TestSeeder';
+
+	/**
+	 * The path to where we can find the test Seeds directory.
+	 *
+	 * @var string
+	 */
+	protected $basePath = SUPPORTPATH . 'Database/';
+
+	/**
+	 * The namespace to help us find the migration classes.
+	 *
+	 * @var string
+	 */
+	protected $namespace = 'App';
+
+	public function setUp(): void
+	{
+		parent::setUp();
+	}
+}
