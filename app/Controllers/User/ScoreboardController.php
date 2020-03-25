0df2035d56634fdb3493b889b253b4de55f8f8d5
Süleyman ERGEN <hatsat32@protonmail.com>
2020-03-25 22:32:07 +0300

    scoreboard improved

--- app/Controllers/User/ScoreboardController.php
+++ app/Controllers/User/ScoreboardController.php
@@ -4,6 +4,7 @@ use App\Core\UserController;
 use App\Models\ChallengeModel;
 use App\Models\SolvesModel;
 use App\Models\TeamModel;
+use App\Libraries\Scoreboard;
 
 class ScoreboardController extends UserController
 {
@@ -28,6 +29,8 @@ class ScoreboardController extends UserController
 		 * this implementation will sucks
 		 * but before that time I will research better implementation
 		 */
+
+		$scorelib = new Scoreboard();
 
 		if ($scores = cache('scores'))
 		{
@@ -59,98 +62,14 @@ class ScoreboardController extends UserController
 				->where('teams.deleted_at', null)
 				->findAll();
 
-		foreach ($challenges as $ch => $challenge)
-		{
-			$challenges[$ch]->d_point = $this->calculatePoint($challenge->point, $challenge->solve_count);
-		}
+		$challenges = $scorelib->cacculateChallengePoint($challenges);
 
-		foreach ($teamScores as $tm => $team)
-		{
-			$total_point = 0;
+		$teamScores = $scorelib->calculateTeamScores($teamScores, $challenges, $solves);
 
-			$team_solves = array_column($this->teamSolves($solves, $team->id), 'challenge_id');
-
-			foreach ($challenges as $ch => $challenge)
-			{
-				if (in_array($challenge->id, $team_solves))
-				{
-					if ($challenge->type === 'dynamic')
-					{
-						$total_point += $challenge->d_point;
-					}
-					else
-					{
-						$total_point += $challenge->point;
-					}
-				}
-			}
-
-			$teamScores[$tm]->total_point = $total_point;
-
-			$teamScores[$tm]->final = $teamScores[$tm]->total_point - $teamScores[$tm]->cost_sum;
-		}
-
-		// sort by rating or solve time
-		usort($teamScores, function($a, $b) {
-			$retval =  $b->final <=> $a->final;
-
-			if ($retval == 0)
-			{
-				$retval = $a->lastsolve <=> $b->lastsolve;
-			}
-
-			return $retval;
-		});
+		$scorelib->sort($teamScores);
 
 		cache()->save("scores", $teamScores, MINUTE * 5);
 		return $this->render('scoreboard', ['scores' => $teamScores]);
-	}
-
-	//--------------------------------------------------------------------
-
-	/**
-	 * calculates dynamic point for challenge.
-	 * 
-	 * @param int $point
-	 * @param int $solveCount
-	 * @param int $decay
-	 * 
-	 * @return int
-	 */
-	protected function calculatePoint(int $point, int $solveCount, int $decay = 5)
-	{
-		$minimum = $point / 2;
-		$solveCount -= 1;
-
-		$value = (
-			($minimum - $point) / ($decay ** 2) * ($solveCount ** 2)
-		) + $point;
-
-		if ($value < $minimum)
-		{
-			$value = $minimum;
-		}
-
-		return ceil($value);
-	}
-
-	//--------------------------------------------------------------------
-
-	/**
-	 * return solved challenges for certain team
-	 * 
-	 * @param array $solves
-	 * @param int $teamID
-	 * 
-	 * @return array
-	 */
-	protected function teamSolves(array $solves, int $teamID)
-	{
-		$teamSolves = array_filter($solves, function ($solve) use ($teamID) {
-			return $solve->team_id == $teamID;
-		});
-
-		return $teamSolves;
 	}
 
 	//--------------------------------------------------------------------
--- app/Libraries/Scoreboard.php
+++ app/Libraries/Scoreboard.php
@@ -22,6 +22,70 @@ class Scoreboard
 
 	//--------------------------------------------------------------------
 
+	public function sort($scores)
+	{
+		// sort by rating or solve time
+		usort($scores, function($a, $b) {
+			$retval =  $b->final <=> $a->final;
+
+			if ($retval == 0)
+			{
+				$retval = $a->lastsolve <=> $b->lastsolve;
+			}
+
+			return $retval;
+		});
+
+		return $scores;
+	}
+
+	//--------------------------------------------------------------------
+
+	public function calculateTeamScores($teamScores, $challenges, $solves)
+	{
+		foreach ($teamScores as $tm => $team)
+		{
+			$total_point = 0;
+
+			$team_solves = array_column($this->teamSolves($solves, $team->id), 'challenge_id');
+
+			foreach ($challenges as $ch => $challenge)
+			{
+				if (in_array($challenge->id, $team_solves))
+				{
+					if ($challenge->type === 'dynamic')
+					{
+						$total_point += $challenge->d_point;
+					}
+					else
+					{
+						$total_point += $challenge->point;
+					}
+				}
+			}
+
+			$teamScores[$tm]->total_point = $total_point;
+
+			$teamScores[$tm]->final = $teamScores[$tm]->total_point - $teamScores[$tm]->cost_sum;
+		}
+
+		return $teamScores;
+	}
+
+	//--------------------------------------------------------------------
+
+	public function cacculateChallengePoint($challenges)
+	{
+		foreach ($challenges as $i => $challenge)
+		{
+			$challenges[$i]->d_point = $this->calculatePoint($challenge->point, $challenge->solve_count);
+		}
+
+		return $challenges;
+	}
+
+	//--------------------------------------------------------------------
+
 	/**
 	 * calculates dynamic point for challenge.
 	 * 
