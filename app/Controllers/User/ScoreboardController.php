7cc795500bf2807c348639a28df1161ca6ade9d9
Süleyman ERGEN <hatsat32@protonmail.com>
2020-04-12 02:10:15 +0300

    add scoreboard bar chart

--- app/Controllers/User/ScoreboardController.php
+++ app/Controllers/User/ScoreboardController.php
@@ -14,10 +14,19 @@ class ScoreboardController extends UserController
 		if (! $scores = cache('scores'))
 		{
 			$scores = $scoreboard->scores();
+
+			$scores = array_filter($scores, function($score) {
+				return $score->final != 0;
+			});
+
 			cache()->save("scores", $scores, MINUTE * 5);
 		}
 
-		return $this->render('scoreboard', ['scores' => $scores]);
+		$_scores = array_map(function($score) {
+			return ['name' => $score->name, 'score' => $score->final];
+		}, $scores);
+
+		return $this->render('scoreboard', ['scores' => $scores, 'chartdata' => json_encode($_scores)]);
 	}
 
 	//--------------------------------------------------------------------
--- app/Views/default/scoreboard.php
+++ app/Views/default/scoreboard.php
@@ -12,6 +12,10 @@
 		<h1><?= lang('Home.scoreboard') ?></h1>
 	</div>
 
+	<div class="my-2">
+		<canvas id="scoreboard-chart" height="100"></canvas>
+	</div>
+
 	<div class="m-2">
 		<table class="table table-hover">
 			<thead>
@@ -30,5 +34,88 @@
 			</tbody>
 		</table>
 	</div>
+
+	<div id="chart-data" style="display: none"><?= esc($chartdata) ?></div>
+
+	<script src="/_admin/js/Chart.min.js"></script>
+
+	<script>
+
+		colors = {
+			background: [
+				'rgba(54, 162, 235, 0.2)',
+				'rgba(255, 99, 132, 0.2)',
+				'rgba(75, 192, 192, 0.2)',
+				'rgba(255, 206, 86, 0.2)',
+				'rgba(153, 102, 255, 0.2)',
+				'rgba(255, 159, 64, 0.2)',
+				'rgba(54, 162, 235, 0.2)',
+				'rgba(255, 99, 132, 0.2)',
+				'rgba(75, 192, 192, 0.2)',
+				'rgba(255, 206, 86, 0.2)',
+				'rgba(153, 102, 255, 0.2)',
+				'rgba(255, 159, 64, 0.2)',
+				'rgba(54, 162, 235, 0.2)',
+				'rgba(255, 99, 132, 0.2)',
+				'rgba(75, 192, 192, 0.2)',
+				'rgba(255, 206, 86, 0.2)',
+				'rgba(153, 102, 255, 0.2)',
+				'rgba(255, 159, 64, 0.2)',
+				'rgba(54, 162, 235, 0.2)',
+				'rgba(255, 99, 132, 0.2)',
+			],
+			border: [
+				'rgba(54, 162, 235, 1)',
+				'rgba(255, 99, 132, 1)',
+				'rgba(75, 192, 192, 1)',
+				'rgba(255, 206, 86, 1)',
+				'rgba(153, 102, 255, 1)',
+				'rgba(255, 159, 64, 1)',
+				'rgba(54, 162, 235, 1)',
+				'rgba(255, 99, 132, 1)',
+				'rgba(75, 192, 192, 1)',
+				'rgba(255, 206, 86, 1)',
+				'rgba(153, 102, 255, 1)',
+				'rgba(255, 159, 64, 1)',
+				'rgba(54, 162, 235, 1)',
+				'rgba(255, 99, 132, 1)',
+				'rgba(75, 192, 192, 1)',
+				'rgba(255, 206, 86, 1)',
+				'rgba(153, 102, 255, 1)',
+				'rgba(255, 159, 64, 1)',
+				'rgba(54, 162, 235, 1)',
+				'rgba(255, 99, 132, 1)',
+			]
+		}
+
+		data = JSON.parse($("#chart-data").text()).slice(0, 20);
+		team_names  = data.map((d) => { return d.name });
+		team_scores = data.map((d) => { return d.score });
+
+		var myChart = new Chart($("#scoreboard-chart"), {
+			type: 'bar',
+			data: {
+				labels: team_names,
+				datasets: [{
+					data: team_scores,
+					backgroundColor: colors.background,
+					borderColor: colors.border,
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero: true
+						}
+					}]
+				},
+				legend: {
+					display: false
+				},
+			}
+		});
+	</script>
 
 <?= $this->endSection() ?>
